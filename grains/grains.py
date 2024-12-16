32dc7b446adf71c61b0fd17dacb74c158636d6e8
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-12-16 21:39:43 +0100

    grains algo improvement

--- grains/grains.py
+++ grains/grains.py
@@ -2,18 +2,8 @@ def square(number):
     if number <= 0 or number > 64:
         raise ValueError("square must be between 1 and 64")
 
-    result = 1
-    for i in range(1, number):
-        result = result * 2
-
-    return result
+    return pow(2, number - 1)
 
 
 def total():
-    total_grains = 0
-    grains_on_current_square = 1
-    for i in range(64):
-        total_grains += grains_on_current_square
-        grains_on_current_square *= 2
-
-    return total_grains
+    return pow(2, 64) - 1
