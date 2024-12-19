dabbfd18af3d019bb6a8c8f781030c6a61072b16
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-12-19 20:39:20 +0100

    in wip algo

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,3 +1,9 @@
+def violate_equality_triangle(sides):
+    sides_sorted = sorted(sides)
+
+    return sides_sorted[0] + sides_sorted[1] <= sides_sorted[2]
+
+
 def is_a_triangle(sides):
     side = list(set(sides))[0]
 
@@ -8,7 +14,7 @@ def is_a_triangle(sides):
 
 # An _equilateral_ triangle has all three sides the same length.
 def equilateral(sides):
-    if not is_a_triangle(sides):
+    if not is_a_triangle(sides) or violate_equality_triangle(sides):
         return False
 
     if len(list(set(sides))) == 1:
@@ -19,7 +25,7 @@ def equilateral(sides):
 
 # A _scalene_ triangle has all sides of different lengths.
 def scalene(sides):
-    if not is_a_triangle(sides):
+    if not is_a_triangle(sides) or violate_equality_triangle(sides):
         return False
 
     if len(list(set(sides))) == 3:
@@ -30,10 +36,10 @@ def scalene(sides):
 
 # An _isosceles_ triangle has at least two sides the same length.
 def isosceles(sides):
-    if not is_a_triangle(sides):
+    if not is_a_triangle(sides) or violate_equality_triangle(sides):
         return False
 
-    if not scalene(sides) and not equilateral(sides):
+    if not scalene(sides):
         return True
 
     return False
