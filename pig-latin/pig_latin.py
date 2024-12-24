5a3c69075846af02ee76288097608031657fa737
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-12-24 21:07:35 +0100

    algo: begin with vowel in wip

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,4 +1,10 @@
-
+# If a word begins with a vowel, or starts with `"xr"` or `"yt"`, add an `"ay"` sound to the end of the word.
+#
+# For example:
+#
+# - `"apple"` -> `"appleay"` (starts with vowel)
+# - `"xray"` -> `"xrayay"` (starts with `"xr"`)
+# - `"yttria"` -> `"yttriaay"` (starts with `"yt"`)
 def begin_with_vowel(text):
     vowels = ["a", "e", "i", "o", "u"]
     if text[0] in vowels:
@@ -18,16 +24,37 @@ def begin_with_yt(text):
 
     return False
 
-# If a word begins with a vowel, or starts with `"xr"` or `"yt"`, add an `"ay"` sound to the end of the word.
+# If a word begins with one or more consonants, first move those consonants to the end of the word and then add an `"ay"` sound to the end of the word.
 #
 # For example:
 #
-# - `"apple"` -> `"appleay"` (starts with vowel)
-# - `"xray"` -> `"xrayay"` (starts with `"xr"`)
-# - `"yttria"` -> `"yttriaay"` (starts with `"yt"`)
+# - `"pig"` -> `"igp"` -> `"igpay"` (starts with single consonant)
+# - `"chair"` -> `"airch"` -> `"airchay"` (starts with multiple consonants)
+# - `"thrush"` -> `"ushthr"` -> `"ushthray"` (starts with multiple consonants)
+
+def begin_with_consonants(text):
+    vowels = ["a", "e", "i", "o", "u"]
+    result = ""
+
+    for index, letter in enumerate(text):
+        if letter in vowels:
+            break
+        result = result + letter
+
+    if len(result) > 0:
+        return text[len(result):] + result
+
+    return False
+
+
+
 def translate(text):
+    if bool(begin_with_consonants(text)) and not begin_with_xr(text) and not begin_with_yt(text):
+        return begin_with_consonants(text) + "ay"
+
     if begin_with_vowel(text) or begin_with_xr(text) or begin_with_yt(text):
         return text + "ay"
+
 
 
     return text
--- pig-latin/pig_latin_tdd_test.py
+++ pig-latin/pig_latin_tdd_test.py
@@ -8,7 +8,8 @@ from pig_latin import (
     translate,
     begin_with_vowel,
     begin_with_xr,
-    begin_with_yt
+    begin_with_yt,
+    begin_with_consonants
 )
 
 class WordBeginWithVowel(unittest.TestCase):
@@ -32,6 +33,13 @@ class WordBeginWithYT(unittest.TestCase):
     def test_word_not_begin_with_yt(self):
         self.assertEqual(begin_with_yt("eday"), False)
 
+class WordBeginWithConsonants(unittest.TestCase):
+    def test_word_begin_with_consonants(self):
+        self.assertEqual(begin_with_consonants("mliay"), "iayml")
+
+    def test_word_not_begin_with_consonants(self):
+        self.assertEqual(begin_with_consonants("iay"), False)
+
 
 class PigLatinTest(unittest.TestCase):
     def test_word_beginning_with_a(self):
@@ -40,8 +48,20 @@ class PigLatinTest(unittest.TestCase):
     def test_word_beginning_with_xr(self):
         self.assertEqual(translate("xray"), "xrayay")
 
+    def test_word_begin_with_ml(self):
+        self.assertEqual(translate("mliay"), "iaymlay")
+
     def test_word_beginning_with_yt(self):
         self.assertEqual(translate("ytay"), "ytayay")
+
+    def test_word_thrush(self):
+        self.assertEqual(translate("thrush"), "ushthray")
+
+    def test_word_chair(self):
+        self.assertEqual(translate("chair"), "airchay")
+
+
+
 
 
 
