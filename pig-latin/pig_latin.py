ce41be9e7ff1193fff3f641464e9ff19f8446caa
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2024-12-24 20:31:44 +0100

    algo: begin with vowel

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,33 @@
+
+def begin_with_vowel(text):
+    vowels = ["a", "e", "i", "o", "u"]
+    if text[0] in vowels:
+        return text[0]
+
+    return False
+
+def begin_with_xr(text):
+    if text[:2] == "xr":
+        return True
+
+    return False
+
+def begin_with_yt(text):
+    if text[:2] == "yt":
+        return True
+
+    return False
+
+# If a word begins with a vowel, or starts with `"xr"` or `"yt"`, add an `"ay"` sound to the end of the word.
+#
+# For example:
+#
+# - `"apple"` -> `"appleay"` (starts with vowel)
+# - `"xray"` -> `"xrayay"` (starts with `"xr"`)
+# - `"yttria"` -> `"yttriaay"` (starts with `"yt"`)
 def translate(text):
-    pass
+    if begin_with_vowel(text) or begin_with_xr(text) or begin_with_yt(text):
+        return text + "ay"
+
+
+    return text
